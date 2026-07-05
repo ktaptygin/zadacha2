a7233ff2afc009fd0d849601ea4772d4aae22171
ktaptygin <kostiktt2006@gmail.com>
2026-07-05 17:42:42 +0300

    revision of the main page (screen). still not adapted for mobile devices.

--- restaurant/static/restaurant/js/main.js
+++ restaurant/static/restaurant/js/main.js
@@ -1,3 +1,13 @@
 $(document).ready(function () {
+    $('a[href^="#"]').click(function (event) {
+        var block = $(this).attr('href');
 
+        if ($(block).length) {
+            event.preventDefault();
+
+            $('html, body').animate({
+                scrollTop: $(block).offset().top
+            }, 600);
+        }
+    });
 });
