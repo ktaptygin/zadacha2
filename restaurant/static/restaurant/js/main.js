ff4dbd1221666874e0b17e0a7997edd16d23f778
ktaptygin <kostiktt2006@gmail.com>
2026-07-11 12:02:16 +0300

    all sections from the layout have been added. all options and buttons have been included. beta 2 of the mobile version

--- restaurant/static/restaurant/js/main.js
+++ restaurant/static/restaurant/js/main.js
@@ -11,3 +11,28 @@ $(document).ready(function () {
         }
     });
 });
+
+$(document).ready(function () {
+    $('.menu__category').on('click', function () {
+        var category = $(this).data('category');
+        var isActive = $(this).hasClass('is-active');
+
+        $('.menu__category').removeClass('is-active');
+
+        if (isActive) {
+            $('.menu__item').removeClass('is-hidden');
+            return;
+        }
+
+        $(this).addClass('is-active');
+
+        $('.menu__item').each(function () {
+            if ($(this).data('category') === category) {
+                $(this).removeClass('is-hidden');
+            } else {
+                $(this).addClass('is-hidden');
+            }
+        });
+    });
+});
+
